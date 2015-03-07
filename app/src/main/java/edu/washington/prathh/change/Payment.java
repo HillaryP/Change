937f844a01a07ca135c178fa9fae79931d64962e
Lily Dwyer <lmdwyer@uw.edu>
2015-03-07 01:19:34 -0800

    Added framework for screens

--- app/src/main/java/edu/washington/prathh/change/Payment.java
+++ app/src/main/java/edu/washington/prathh/change/Payment.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.content.Context;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 import org.w3c.dom.Text;
 
@@ -13,18 +18,36 @@ import java.util.List;
 
 
 public class Payment extends ActionBarActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
-        Uri data = getIntent().getData();
-        List<String> params = data.getPathSegments();
-        String first = params.get(0);
-        TextView view = (TextView) findViewById(R.id.user_info);
-        view.setText(first);
+//        Uri data = getIntent().getData();
+//        List<String> params = data.getPathSegments();
+//        String first = params.get(0);
+//        TextView view = (TextView) findViewById(R.id.user_info);
+//        view.setText(first);
+        addListenerOnButton();
     }
 
+    public void addListenerOnButton() {
+        final Context context = this;
+        button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, Payment_Step2.class);
+                startActivity(intent);
+
+            }
+
+        });
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
