c3f5758cc0f55b443fb5a096280160a25dfd07cd
Hillary Prather <prathh@uw.edu>
2015-03-07 02:57:01 -0800

    added ability to make httprequest and get resulting JSON

--- app/src/main/java/edu/washington/prathh/change/Payment.java
+++ app/src/main/java/edu/washington/prathh/change/Payment.java
@@ -1,8 +1,10 @@
 package edu.washington.prathh.change;
 
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -11,13 +13,23 @@ import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.util.List;
 
 
 public class Payment extends ActionBarActivity {
+    public final String ME_URL = "https://api.venmo.com/v1/me?access_token=";
     Button button;
 
     @Override
@@ -27,12 +39,7 @@ public class Payment extends ActionBarActivity {
         Uri data = getIntent().getData();
         String token = data.getQueryParameter("access_token");
         ((ChangeApp)getApplication()).setAccessToken(token);
-        TextView view = (TextView) findViewById(R.id.user_info);
-        if (token != null) {
-            view.setText(token);
-        } else {
-            view.setText("Something went wrong while trying to access this account. Please try again later.");
-        }
+        new RequestTask().execute(ME_URL + token);
     }
 
     public void addListenerOnButton() {
@@ -72,5 +79,33 @@ public class Payment extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class RequestTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... uri) {
+            if (uri[0] != null) {
+                try {
+                    HttpClient httpClient = new DefaultHttpClient();
+                    Log.i("Payment", "Attempting to request: " + uri[0]);
+                    HttpResponse response = httpClient.execute(new HttpGet(uri[0]));
+                    StatusLine statusLine = response.getStatusLine();
+                    if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
+                        String jsonString = EntityUtils.toString(response.getEntity());
+                        JSONObject json = new JSONObject(jsonString);
+                        return "Welcome to Change, " + json.getJSONObject("data").getJSONObject("user").getString("first_name");
+                    }
+                } catch (Exception e) {
+                    Log.w("Payment", "Something went wrong while getting url response: " + e);
+                }
+            }
+            return "Something went wrong with your request";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Toast.makeText(Payment.this, result, Toast.LENGTH_LONG).show();
+        }
     }
 }
